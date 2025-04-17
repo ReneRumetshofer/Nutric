0fb7331458e17f0afd8376da06798d33746ac019
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2025-04-17 20:32:42 +0200

    start restyling meal cards

--- frontend/src/app/components/dashboard/meal-card/meal-card.component.ts
+++ frontend/src/app/components/dashboard/meal-card/meal-card.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Card } from 'primeng/card';
 import { Button } from 'primeng/button';
 import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
+import { Panel } from 'primeng/panel';
 
 @Component({
   selector: 'app-meal-card',
-  imports: [Card, Button, ProgressBarComponent],
+  imports: [Card, Button, ProgressBarComponent, Panel],
   templateUrl: './meal-card.component.html',
   standalone: true,
   styleUrl: './meal-card.component.scss',
