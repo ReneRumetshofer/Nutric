5c9a07e7e5cde9ce9b41153898760ce179d29d24
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2025-05-02 20:53:52 +0200

    update current day on profile update

--- backend/src/main/java/dev/rumetshofer/nutric/use_cases/UpdateProfileUseCase.java
+++ backend/src/main/java/dev/rumetshofer/nutric/use_cases/UpdateProfileUseCase.java
@@ -1,32 +1,55 @@
 package dev.rumetshofer.nutric.use_cases;
 
+import dev.rumetshofer.nutric.out.db.entities.DayDbModel;
 import dev.rumetshofer.nutric.out.db.entities.ProfileDbModel;
+import dev.rumetshofer.nutric.out.db.repositories.DayRepository;
 import dev.rumetshofer.nutric.out.db.repositories.ProfileRepository;
 import dev.rumetshofer.nutric.use_cases.dto.in.UpdateProfileRequest;
 import dev.rumetshofer.nutric.use_cases.exceptions.ProfileNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Component
 public class UpdateProfileUseCase {
 
     private final ProfileRepository profileRepository;
+    private final DayRepository dayRepository;
 
-    public UpdateProfileUseCase(ProfileRepository profileRepository) {
+    public UpdateProfileUseCase(ProfileRepository profileRepository, DayRepository dayRepository) {
         this.profileRepository = profileRepository;
+        this.dayRepository = dayRepository;
     }
 
     public void update(UUID userUuid, UpdateProfileRequest updateProfileRequest) {
         ProfileDbModel foundProfileDbModel = profileRepository.findById(userUuid)
                 .orElseThrow(() -> new ProfileNotFoundException(userUuid));
 
-        foundProfileDbModel.setWeight(updateProfileRequest.weight());
-        foundProfileDbModel.setCalorieGoal(updateProfileRequest.calorieGoal());
-        foundProfileDbModel.setCarbLimitGrams(updateProfileRequest.carbLimitGrams());
-        foundProfileDbModel.setProteinLimitGrams(updateProfileRequest.proteinLimitGrams());
-        foundProfileDbModel.setFatLimitGrams(updateProfileRequest.fatLimitGrams());
-        profileRepository.save(foundProfileDbModel);
+        updateProfile(foundProfileDbModel, updateProfileRequest);
+
+        dayRepository.findByDayAndUserUuid(LocalDate.now(), userUuid).ifPresent(dayDbModel ->
+            updateCurrentDay(dayDbModel, updateProfileRequest)
+        );
+    }
+
+    private void updateProfile(ProfileDbModel profileDbModel, UpdateProfileRequest updateProfileRequest) {
+        profileDbModel.setWeight(updateProfileRequest.weight());
+        profileDbModel.setCalorieGoal(updateProfileRequest.calorieGoal());
+        profileDbModel.setCarbLimitGrams(updateProfileRequest.carbLimitGrams());
+        profileDbModel.setProteinLimitGrams(updateProfileRequest.proteinLimitGrams());
+        profileDbModel.setFatLimitGrams(updateProfileRequest.fatLimitGrams());
+
+        profileRepository.save(profileDbModel);
+    }
+
+    private void updateCurrentDay(DayDbModel dayDbModel, UpdateProfileRequest updateProfileRequest) {
+        dayDbModel.setCalorieGoal(updateProfileRequest.calorieGoal());
+        dayDbModel.setCarbLimitGrams(updateProfileRequest.carbLimitGrams());
+        dayDbModel.setProteinLimitGrams(updateProfileRequest.proteinLimitGrams());
+        dayDbModel.setFatLimitGrams(updateProfileRequest.fatLimitGrams());
+
+        dayRepository.save(dayDbModel);
     }
 
 }
