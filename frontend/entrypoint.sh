5525f820668b0e5a77a84c9e70e4e5968c1f0c0c
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2025-05-16 14:25:16 +0200

    dockerize frontend

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+exec "$@"
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,19 +1,23 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { TrackFoodScreenComponent } from './components/track-food-screen/track-food-screen.component';
 import { isAuthenticated } from './guards/auth.guard';
-import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent, canActivate: [isAuthenticated] },
   {
     path: 'track',
-    component: TrackFoodScreenComponent,
+    loadChildren: () =>
+      import('./components/track-food-screen/track-food-screen.component').then(
+        (m) => m.TrackFoodScreenComponent,
+      ),
     canActivate: [isAuthenticated],
   },
   {
     path: 'profile',
-    component: ProfileComponent,
+    loadChildren: () =>
+      import('./components/profile/profile.component').then(
+        (m) => m.ProfileComponent,
+      ),
     canActivate: [isAuthenticated],
   },
 ];
